ad4748d035dde04d87e23223082b54028edceccd
TDWolff <torin.dean.wolff@gmail.com>
2024-05-31 12:30:18 -0700

    Update install_github.sh

--- src/ubuntu/install/github/install_github.sh
+++ src/ubuntu/install/github/install_github.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 set -ex
 
-apt-get update
-wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
-sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
+# Add the GitHub package repository
+wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
 
-sudo apt update && sudo apt install github-desktop
+# Update the package list
+sudo apt-get update
 
+# Install GitHub Desktop
+sudo apt-get install github-desktop
+
+# add to desktop
 cp /usr/share/applications/github-desktop.desktop $HOME/Desktop/
-chmod +x $HOME/Desktop/github-desktop.desktop
 chown 1000:1000 $HOME/Desktop/github-desktop.desktop
+chmod +x $HOME/Desktop/github-desktop.desktop
+
+# add to path
+mv /usr/bin/github-desktop /usr/bin/github-desktop-orig
+cat >/usr/bin/github-desktop <<EOL
+#!/usr/bin/env bash
+exec /usr/share/github-desktop/github-desktop \$@
+EOL
+chmod +x /usr/bin/github-desktop
